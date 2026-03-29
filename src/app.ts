a4981ced1ba895d759c77a13faf20d96871457ec
Valerieee01 <zharmel.afa@gmail.com>
2026-03-29 01:32:21 -0500

    v1.0.1 Karde360

--- src/app.ts
+++ src/app.ts
@@ -7,12 +7,12 @@ import { errorHandler } from "./middlewares/error.middleware";
 const app = express();
 const prisma = new PrismaClient();
 
-
 // Middlewares globales
 app.use(cors());
-app.use(express.json());
 
-
+// ✅ Solo uno, con el límite
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ limit: "10mb", extended: true }));
 
 // Rutas principales
 app.use("/System/Inventory/Information", routes);
